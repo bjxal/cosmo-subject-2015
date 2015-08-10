8b5cb6ae36c014c96c62d2bed0209418432d7087
bjxal@139.com <bjxal@139.com>
2015-08-10 18:50:09 +0800

    eileen

---
+++ subjects/20150810_basha_cishanye/js-subject/index.js
@@ -0,0 +1,299 @@
+var p1List = [
+    {"tit":ImgDir('/p1/t1/tit.png'),"word":'慈善主题：慈善因你而改变<br>捐助项目：思源救护·贫困县/乡卫生院救护车项目、永源基金会·偏远地区乡村学校希望教室项目、思源工程·阿里巴巴净水计划芭莎乡村学校净水项目、西部阳光· 贫困乡村幼儿园项目、天使妈妈·乡村儿童大病救治项目<br>善款总额：5131万',"pic":ImgDir('/p1/t1/pic.jpg')},
+    {"tit":ImgDir('/p1/t2/tit.png'),"word":'慈善主题：1+1 中国梦心启航<br>捐助项目：中华思源工程扶贫基金会芭莎公益慈善基金<br>用于中华思源工程扶贫基金会 —“思源救护”<br>中国扶贫基金会合作 ——“筑巢行动”<br>善款总额：57,221,925',"pic":ImgDir('/p1/t2/pic.jpg')},
+    {"tit":ImgDir('/p1/t3/tit.png'),"word":'慈善主题：让慈善影响中国<br>捐助项目：中华思源工程扶贫基金会，<br>用于北京成龙慈善基金会<br>-贫困儿童大病救治<br>中国红十字基金会嫣然天使基金之颅颜中心<br>-唇腭裂序列治疗<br>华谊兄弟公益基金<br>-“儿童公益短片”项目<br>善款总额：46,735,550',"pic":ImgDir('/p1/t3/pic.jpg')},
+    {"tit":ImgDir('/p1/t4/tit.png'),"word":'慈善主题：让慈善成为希望<br>捐助项目：中华少年儿童慈善救助基金会<br>—“回家的希望”项目<br>北京成龙慈善基金会<br>—贫困大病儿童救治<br>善款总额：32,630,000元',"pic":ImgDir('/p1/t4/pic.jpg')},
+    {"tit":ImgDir('/p1/t5/tit.png'),"word":'慈善主题：世博情 中国心<br>捐助项目：中华社会救助基金会<br>— 用于西南旱灾，青海地震和贫困地区的环保公益项目<br>善款总额：18,828,887元',"pic":ImgDir('/p1/t5/pic.jpg')},
+    {"tit":ImgDir('/p1/t6/tit.png'),"word":'慈善主题：让慈善成为力量<br>捐助项目：北京成龙慈善基金会<br>—用于贫困大病儿童的救治工作<br>善款总额：31,000,000元',"pic":ImgDir('/p1/t6/pic.jpg')},
+    {"tit":ImgDir('/p1/t7/tit.png'),"word":'慈善主题：让慈善成为行动<br>捐助项目：中国红十字会李连杰壹基金计划<br>— 用于汶川地震灾后重建及慈善推广事业<br>善款总额：12,520,000元',"pic":ImgDir('/p1/t7/pic.jpg')},
+    {"tit":ImgDir('/p1/t8/tit.png'),"word":'慈善主题：让慈善创造梦想<br>捐助项目：中国慈善总会 ——慈爱孤儿项目<br>善款总额：7,538,888元',"pic":ImgDir('/p1/t8/pic.jpg')},
+    {"tit":ImgDir('/p1/t9/tit.png'),"word":'慈善主题：让慈善改变命运<br>捐助项目：联合国儿童基金会<br>—“奥迪童梦圆”西部小学建设项目<br>善款总额：3,518,439元',"pic":ImgDir('/p1/t9/pic.jpg')},
+    {"tit":ImgDir('/p1/t10/tit.png'),"word":'慈善主题：让慈善成为习惯<br>捐助项目：北京慈善总会 ——贫困中学生助学项目<br>善款总额：1,724,600元',"pic":ImgDir('/p1/t10/pic.jpg')},
+    {"tit":ImgDir('/p1/t11/tit.png'),"word":'慈善主题：让慈善成为时尚<br>捐助项目：中国妇女发展基金会<br>—西部母亲健康快车项目<br>善款总额：680,000元',"pic":ImgDir('/p1/t11/pic.jpg')},
+    {"tit":ImgDir('/p1/t12/tit.png'),"word":'慈善主题：慈善，到底是谁需要谁？<br>捐助项目：北京市红十字会 — 非典救助<br>善款总额：167,504元',"pic":ImgDir('/p1/t12/pic.jpg')}
+];
+var p2List = [
+    ImgDir('/p2/list/1.jpg'),
+    ImgDir('/p2/list/2.jpg'),
+    ImgDir('/p2/list/3.jpg'),
+    ImgDir('/p2/list/4.jpg'),
+    ImgDir('/p2/list/5.jpg'),
+    ImgDir('/p2/list/6.jpg')
+];
+var p3List = [
+    ImgDir('/p3/list/1.jpg'),
+    ImgDir('/p3/list/2.jpg')
+];
+var p4List = [
+    ImgDir('/p4/list/1.jpg'),
+    ImgDir('/p4/list/2.jpg')
+];
+var p5List = [
+    ImgDir('/p5/list/1.jpg'),
+    ImgDir('/p5/list/2.jpg')
+];
+var p6List = [
+    ImgDir('/p6/list/1.jpg'),
+    ImgDir('/p6/list/2.jpg'),
+    ImgDir('/p6/list/3.jpg'),
+    ImgDir('/p6/list/4.jpg'),
+    ImgDir('/p6/list/5.jpg')
+];
+var p7List = [
+    ImgDir('/p7/list/1.jpg'),
+    ImgDir('/p7/list/2.jpg'),
+    ImgDir('/p7/list/3.jpg'),
+    ImgDir('/p7/list/4.jpg')
+];
+var content = encodeURIComponent('我就是COSMO GIRL'),
+    title  = '',
+    pic   = ImgDir('/weixin.jpg'),
+    url   = '';//encodeURIComponent('http://m.durex.com.cn/qr/1N'),
+back  = encodeURIComponent('http://m.cosmopolitan.com.cn/');
+tourl = '';
+//var url = "http://m.onlylady.com/files/eventapi.php?c=EventNew&a=ZST&indexsId=618";
+move_m = false;
+grade = 0;
+Fui.Template.IMG_DIR = ImgDir();
+var PAGE0 = Fui.Template.extend({
+    config:{
+    },
+    design:function(){
+        $(".fui-arrow").hide();
+    }
+    ,getGestureItems:function(){
+        return[
+            {
+                gesture:"tap",
+                name:"pop_show",
+                callback:function(e,$tar){
+                    //removeImgList();
+                    slider.set("lock",true);
+                    var popid = $tar.data("popid");
+                    setImgList(popid);
+                    $(".pop").addClass("show");
+                    myScroll = new iScroll('wrapper',{
+                        snap: true,
+                        momentum: false,
+                        hScrollbar: false
+                    });
+                }
+            },
+            {
+                gesture:"tap",
+                name:"share_pop_show",
+                callback:function(e,$tar){
+                    $(".share_pop").fadeIn();
+                    $(".app-music").css("z-index","-1");
+                    slider.set("lock","true");
+                }
+            },
+            {
+                gesture:"tap",
+                name:"share_pop_hide",
+                callback:function(e,$tar){
+                    $(".share_pop").fadeOut();
+                    $(".app-music").css("z-index","101");
+                    slider.set("lock","false");
+                }
+            },
+            {
+                gesture:"tap",
+                name:"share_sina",
+                callback:function(e,$tar){
+                    tourl = 'http://service.weibo.com/share/mobile.php?title=$content&url=$url&pic=$pic&backurl=$back';
+                    tourl = tourl.replace('$title',title).replace('$content',content).replace('$pic',pic).replace('$url',url).replace('$back',back);
+                    window.open(tourl,'_blank');
+                }
+            },{
+                gesture:"tap",
+                name:"m_link",
+                callback:function(e,$tar){
+                    var url = "http://m.cosmopolitan.com.cn/";
+                    window.open(url,'_blank');
+                }
+            }
+        ]
+    }
+});
+Fui.Template.regTpl({
+    PAGE0:PAGE0
+});
+//var audio = Fui.Audio({
+//    src:ImgDir('/music.mp3'),
+//    color:"#ee257b",
+//    autoplay:false
+//});
+var slider = new Fui.PageSlider({
+    el:'#pack',
+    curPage:0,
+    lock:false,
+    iteration:false,
+    orient:'y',
+    listeners:{
+        slide:function(){
+            move_t = false;
+            var gesture = slider.event.gesture;
+            $(".fui-arrow.right").hide();
+            var page = this.get("curPage");
+            $(".p"+page).addClass("focus");
+            if(page==9){
+                $(".fui-arrow").css("z-index","-1");
+                $(".app-music").css("z-index","-1");
+            }
+            else{
+
+                $(".fui-arrow").css("z-index","10000");
+                $(".app-music").css("z-index","101");
+            }
+            if(page==2){
+                $('.num_1').each(count);
+                $('.num_2').each(count);
+                $('.num_3').each(count);
+                $('.num_4').each(count);
+            }
+            if(page==8){
+                var cname = $(".p4 .tips").attr("class");
+                if(cname.indexOf("hide")==-1){
+                    $(".p4 .tips").fadeIn();
+                    setTimeout(function(){$(".p4 .tips").fadeOut().addClass("hide");},1000);
+                }
+            }
+            else{
+                $(".p4 .tips").hide();
+            }
+        }
+    },
+    data:[
+        {
+            template:'PAGE0',
+            xtpl:'p0',
+            design:function(){
+                var aa = this;
+                //setTimeout(function(){
+                //    aa.$el.find(".loadingC").hide();
+                //    aa.$el.addClass("focus");
+                //    //audio.play();
+                //    $(".fui-arrow").css("z-index","1");
+                //    setTimeout(function(){
+                //        aa.$el.find(".loading").fadeOut();
+                //        aa.$el.find(".actBox").addClass("show");
+                //        $('.timer').each(count);
+                //        // custom formatting example
+                //        $('#count-number').data('countToOptions', {
+                //            formatter: function (value, options) {
+                //                return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+                //            }
+                //        });
+                //    },3000);
+                //},5000);
+            }
+        }
+        ,{
+            template:'PAGE0',
+            xtpl:'p1'
+        }
+        ,{
+            template:'PAGE0',
+            xtpl:'p2'
+        }
+        ,{
+            template:'PAGE0',
+            xtpl:'p3'
+        }
+        ,{
+            template:'PAGE0',
+            xtpl:'p4'
+        }
+        ,{
+            template:'PAGE0',
+            xtpl:'p5'
+        }
+        , {
+            template: 'PAGE0',
+            xtpl: 'p6'
+        }
+        , {
+            template: 'PAGE0',
+            xtpl: 'p7'
+        }
+        , {
+            template: 'PAGE0',
+            xtpl: 'p8'
+        }
+    ]
+});
+slider.render();
+//$(".fui-arrow").css("z-index","-1");
+//cover
+$(".cover").on("touchend",function(e){
+    $(".cover").fadeOut();
+});
+$(".close").bind("touchend",function(e){
+    var tar = e.target;
+    removeImgList(myScroll);
+    slider.set("lock",false);
+    $(tar).parent().removeClass("show");
+});
+
+function removeImgList(myScroll){
+    myScroll.destroy();
+    $("#thelist").empty();
+}
+function setImgList(id){
+    var arr = p1List;
+    switch (id){
+        case 1:
+            arr = p1List;
+            break;
+        case 2:
+            arr = p2List;
+            break;
+        case 3:
+            arr = p3List;
+            break;
+        case 4:
+            arr = p4List;
+            break;
+        case 5:
+            arr = p5List;
+            break;
+        case 6:
+            arr = p6List;
+            break;
+        case 7:
+            arr = p7List;
+            break;
+    }
+    $.each(arr,function(i,item){
+        var li1 = '<li><img src="'+item.tit+'"/><p>'+item.word+'</p><img src="'+item.pic+'"</li>';
+        $("#thelist").append(li1);
+    });
+    $("#scroller").css("width",arr.length*540+"px");
+}
+var picList = [
+    {
+        width: 750,
+        height: 207,
+        content: ImgDir('/p4/1.jpg')
+    },
+    {
+        width: 750,
+        height: 207,
+        content: ImgDir('/p4/2.jpg')
+    },
+    {
+        width: 750,
+        height: 207,
+        content: ImgDir('/p4/3.jpg')
+    }
+];
+var islider1 = new iSlider({
+    data: picList,
+    dom: document.getElementById("animation-effect"),
+    duration: 3000,
+    animateType: 'rotate',
+    isAutoplay: false,
+    isLooping: true,
+    //isVertical: true// 是否垂直滚动
+});
