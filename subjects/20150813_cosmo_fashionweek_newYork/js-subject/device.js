86ee13cf11dfe982f06301c8bfd0163d85c2a3d3
bjxal@139.com <bjxal@139.com>
2015-08-17 18:14:53 +0800

    eileen

--- subjects/20150813_cosmo_fashionweek_newYork/js-subject/device.js
+++ subjects/20150813_cosmo_fashionweek_newYork/js-subject/device.js
@@ -1,13 +1,13 @@
 ;(function(){if(/Android (\d+\.\d+)/.test(navigator.userAgent)){
     var version = parseFloat(RegExp.$1);
     if(version>2.3){
-        var phoneScale = parseInt(window.screen.width)/750;
-        document.write('<meta name="viewport" content="width=750, minimum-scale = '+ phoneScale +', maximum-scale = '+ phoneScale +', target-densitydpi=device-dpi">');
+        var phoneScale = parseInt(window.screen.width)/450;
+        document.write('<meta name="viewport" content="width=450, minimum-scale = '+ phoneScale +', maximum-scale = '+ phoneScale +', target-densitydpi=device-dpi">');
     }else{
-        document.write('<meta name="viewport" content="width=750, target-densitydpi=device-dpi">');
+        document.write('<meta name="viewport" content="width=450, target-densitydpi=device-dpi">');
     }
 }else{
-    document.write('<meta name="viewport" content="width=750, user-scalable=no">');
+    document.write('<meta name="viewport" content="width=450, user-scalable=no">');
 }
 //微信去掉下方刷新栏
     if(navigator.userAgent.indexOf('MicroMessenger') >= 0){
--- subjects/20150813_cosmo_fashionweek_newYork/js-subject/index.js
+++ subjects/20150813_cosmo_fashionweek_newYork/js-subject/index.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
+    $("html,body").animate({
+        scrollTop: "10000px"
+    },800);
     /*footer nav*/
     $(".nav_word").click("touchend",function(e){
         var $tar = $(e.target);
-        console.log($tar)
         $tar.next().toggleClass("act");
+        $tar.parent().siblings().find(".nav_list").removeClass("act");
     });
 
     /*sina*/
@@ -11,4 +14,22 @@ $(document).ready(function(){
         $(".footer_nav_list").removeClass("show");
         $(".sina_p").addClass("show");
     });
+
+    /*audio*/
+    var audioPlay = $("#audioPlay")[0];
+    $(".audio_c").on("touchend",function(e){
+        audioPlay.play();
+        $(".audio_time span").eq(0).addClass("read");
+    });
+
+    /*to date*/
+    $(".date li a").on("touchend",function(e){
+        $(e.target).parents(".nav_list").removeClass("act");
+        var id = $(e.target).data("id");
+        var topVal = $("#date"+id).offset().top;
+        $("html,body").animate({
+            scrollTop: (topVal-20)+"px"
+        },function(){});
+
+    })
 });
