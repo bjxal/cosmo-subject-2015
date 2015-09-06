32ed2a5f41aef2b84bb09e107a7f5738bc691f56
bjxal@139.com <bjxal@139.com>
2015-09-06 19:00:26 +0800

    eileen

--- subjects/20150813_cosmo_fashionweek_newYork/js-subject/index.js
+++ subjects/20150813_cosmo_fashionweek_newYork/js-subject/index.js
@@ -14,8 +14,16 @@ $(document).ready(function(){
         $tar.parent().siblings().find(".sina_tips").removeClass("act");
     });
     /*audio*/
-    var audioPlay = $("#audioPlay")[0];
+    setTimeout(function(){
+        $.each($(".audio_c_1"),function(i,item){
+            var $par = $(item).parents(".audio");
+            var audioPlay1 = $par.find("audio")[0];
+            $par.find(".audio_time span").eq(1).html(Math.floor(audioPlay1.duration)+'"');
+        });},1000);
     $(".audio_c_1").on("touchend",function(e){
+        var $tar = $(e.target);
+        var $par = $tar.parents(".audio");
+        var audioPlay = $par.find("audio")[0];
         var ifPlay = $(e.target).attr("class");
         if(ifPlay.indexOf("play")!=-1){
             audioPlay.pause();
@@ -27,8 +35,9 @@ $(document).ready(function(){
             //$(".audio_c_1").removeClass("play");
             $(e.target).addClass("play");
         }
-        $(".audio_time span").eq(0).addClass("read");
-        $(".nav_list,.sina_tips").removeClass("act");
+        console.log(audioPlay.duration);
+        $par.find(".audio_time span").eq(0).addClass("read");
+        $par.find(".nav_list,.sina_tips").removeClass("act");
     });
 
     /*to date*/
