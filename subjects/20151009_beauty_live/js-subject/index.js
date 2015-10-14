540eb2d4803c05afde82210c39f4f4207dd8585a
bjxal@139.com <bjxal@139.com>
2015-10-14 11:31:47 +0800

    eileen

--- subjects/20151009_beauty_live/js-subject/index.js
+++ subjects/20151009_beauty_live/js-subject/index.js
@@ -1,5 +1,109 @@
 require("/fliza-ui");
 $(".weixin_jpg").attr("src",ImgDir('/weixin.jpg'));
+LIST = function(){}
+LIST.prototype = {
+    set_watch_list:function(){
+        var islider1 = new iSlider({
+            data: watch_list,
+            type:"dom",
+            dom: document.getElementById("animation-effect"),
+            duration: 3000,
+            animateType: 'rotate',
+            isAutoplay: false,
+            isLooping: true,
+            onslideend:function(){
+                var list = $("#animation-effect li");
+                $.each(list,function(i,item){
+                    var deg_v = $(item).attr("style").split("rotateY(")[1].split("deg")[0];
+                    if(deg_v==0){
+                        $(item).find(".word").addClass("cur");
+                    }
+                    else{
+                        $(item).find(".word").removeClass("cur");
+                    }
+                });
+            }
+            //isVertical: true// 是否垂直滚动
+        });
+    },
+    set_star_list:function(){
+        var islider2 = new iSlider({
+            data: star_list,
+            //type:'dom',
+            dom: document.getElementById("animation-effect-2"),
+            duration: 3000,
+            animateType: 'flow',
+            isAutoplay: false,
+            isLooping: true,
+            onslideend:function(){
+                var id = islider2.slideIndex;
+                $(".p2 .name-list p").eq(id).addClass("cur").siblings().removeClass("cur");
+
+            }
+            //isVertical: true// 是否垂直滚动
+        });
+    },
+    set_guests_list:function(){
+        var me = this;
+        /*lf*/
+        $.each(guests_list_lf,function(i,item){
+            var lf_html = me.guests_html(item,i);
+            $(".p3 .list-c-lf").append(lf_html);
+        });
+        /*rt*/
+        $.each(guests_list_rt,function(i,item){
+            var rt_html = me.guests_html(item,i);
+            $(".p3 .list-c-rt").append(rt_html);
+        });
+    },
+    guests_html:function(obj,i){
+        var c = document.createElement("div");
+        c.className = (i==0)?"list-c-item cur":"list-c-item";
+
+        var c_img = document.createElement("img");
+        c_img.src = obj.imgUrl;
+        c.appendChild(c_img);
+
+        var c_name = document.createElement("div");
+        c_name.innerHTML = obj.name;
+        c.appendChild(c_name);
+
+        return c;
+    },
+    set_icon_list:function(){
+        var me = this;
+        $.each(icon_list_lf,function(i,item){
+            var id = i%4+1;
+            var icon_html = me.icon_html(item,i);
+            $(".list-c-"+id).append(icon_html);
+        });
+    },
+    icon_html:function(obj,i){
+        var id = i%4;
+        var c = document.createElement("div");
+        c.className = "item-"+id+"-"+(id%4);
+
+        var c_avt = document.createElement("div");
+        c_avt.className = "c-avt";
+        c_avt.innerHTML = '<img src="'+obj.imgUrl+'"/>';
+        c.appendChild(c_avt);
+
+        var c_info = document.createElement("div");
+        c_info.className = "c-info";
+        c_info.innerHTML = '<h2>'+obj.name+'</h2><p>'+obj.info+'</p>';
+        c.appendChild(c_info);
+
+        var c_line = document.createElement("div");
+        c_line.className = "c-line";
+        c_line.innerHTML = '<span class="line_1"></span><span class="line_2"></span>';
+        c.appendChild(c_line);
+
+        return c;
+
+    },
+    set_moment_list:function(){},
+    set_team_list:function(){}
+};
 Fui.Template.IMG_DIR = ImgDir();
 var rf_id = 0;
 var PAGE0 = Fui.Template.extend({
@@ -28,16 +132,21 @@ var PAGE0 = Fui.Template.extend({
                 callback:function(e,$tar){
                     var $par_lf = $tar.parents(".p").find(".list-c-lf");
                     var $par_rt = $tar.parents(".p").find(".list-c-rt");
-                    rf_id = ((rf_id+1)>=$par_lf.find("img").length)?0:(rf_id+1);
-                    console.log(rf_id)
-                    $par_lf.find("img").eq(rf_id).addClass("cur zIndex cur_"+rf_id);
-                    $par_rt.find("img").eq(rf_id).addClass("cur zIndex cur_"+rf_id);
-                    $par_lf.find("img.cur_"+rf_id).siblings().removeClass("zIndex");
-                    $par_rt.find("img.cur_"+rf_id).siblings().removeClass("zIndex");
+                    rf_id = ((rf_id+1)>=$par_lf.find(".list-c-item").length)?0:(rf_id+1);
+                    $par_lf.find(".list-c-item").eq(rf_id).addClass("cur zIndex cur_"+rf_id);
+                    $par_rt.find(".list-c-item").eq(rf_id).addClass("cur zIndex cur_"+rf_id);
+                    $par_lf.find(".list-c-item.cur_"+rf_id).siblings().removeClass("zIndex");
+                    $par_rt.find(".list-c-item.cur_"+rf_id).siblings().removeClass("zIndex");
                     setTimeout(function(){
-                        $par_lf.find("img.cur_"+rf_id).siblings().removeClass("cur");
-                        $par_rt.find("img.cur_"+rf_id).siblings().removeClass("cur");
+                        $par_lf.find(".list-c-item.cur_"+rf_id).siblings().removeClass("cur");
+                        $par_rt.find(".list-c-item.cur_"+rf_id).siblings().removeClass("cur");
                     },500)
+                }
+            },
+            {
+                gesture:"tap",
+                name:"c-btn-tap",
+                callback:function(e,$tar){
                 }
             }
         ]
@@ -109,82 +218,16 @@ var slider = new Fui.PageSlider({
 slider.render();
 setTimeout(function(){$(".cover").addClass("focus");},1000);
 $(".fui-arrow").removeClass("down");
-var picList_1 = [
-    {
-        'height': '710px',
-        'width': '790px',
-        'content': '<div class="imgs imgs_1"><img src="img-subject/p1/list/1.jpg"/><div class="word cur"><h2>压轴的LV女郎们酷毙了！</h2><p>搭配浪漫唯美的木耳边装饰宫廷风格衬衫，或是摩登时髦的网眼背心，如绅士般潇洒，胸口处的小面积镂空在之前的系列中也曾使用过1</p></div></div>'//ImgDir('/p1/list/1.jpg')
-    },
-    {
-        'height': '710px',
-        'width': '790px',
-        'content': '<div class="imgs imgs_2"><img src="img-subject/p1/list/2.jpg"/><div class="word"><h2>压轴的LV女郎们酷毙了！</h2><p>搭配浪漫唯美的木耳边装饰宫廷风格衬衫，或是摩登时髦的网眼背心，如绅士般潇洒，胸口处的小面积镂空在之前的系列中也曾使用过；那些长度只及下胸2</p></div></div>'//ImgDir('/p1/list/1.jpg')
-    },
-    {
-        'height': '710px',
-        'width': '790px',
-        'content': '<div class="imgs imgs_3"><img src="img-subject/p1/list/3.jpg"/><div class="word"><h2>压轴的LV女郎们酷毙了！</h2><p>搭配浪漫唯美的木耳边装饰宫廷风格衬衫，或是摩登时髦的网眼背心，如绅士般潇洒，胸口处的小面积镂空在之前的系列中也曾使用过；那些长度只及下胸线的短上衣与卖弄柔弱性感。；那些长度只及下胸线的短上衣与卖弄柔弱性感。3</p></div></div>'//ImgDir('/p1/list/1.jpg')
-    },
-    {
-        'height': '710px',
-        'width': '790px',
-        'content': '<div class="imgs"><img src="img-subject/p1/list/4.jpg"/><div class="word"><h2>压轴的LV女郎们酷毙了！</h2><p>搭配浪漫唯美的木耳边装饰4</p></div></div>'
-    }
-];
-var islider1 = new iSlider({
-    data: picList_1,
-    type:"dom",
-    dom: document.getElementById("animation-effect"),
-    duration: 3000,
-    animateType: 'rotate',
-    isAutoplay: false,
-    isLooping: true,
-    onslideend:function(){
-        var list = $("#animation-effect li");
-        $.each(list,function(i,item){
-            var deg_v = $(item).attr("style").split("rotateY(")[1].split("deg")[0];
-            if(deg_v==0){
-                $(item).find(".word").addClass("cur");
-            }
-            else{
-                $(item).find(".word").removeClass("cur");
-            }
-        });
-    }
-    //isVertical: true// 是否垂直滚动
-});
-var picList_2 = [
-    {
-        width:520,
-        height:800,
-        content:ImgDir('/p2/list/1.jpg')
-    },
-    {
-        width:520,
-        height:800,
-        content:ImgDir('/p2/list/2.jpg')
-    },
-    {
-        width:520,
-        height:800,
-        content:ImgDir('/p2/list/3.jpg')
-    }
-];
-var islider2 = new iSlider({
-    data: picList_2,
-    //type:'dom',
-    dom: document.getElementById("animation-effect-2"),
-    duration: 3000,
-    animateType: 'flow',
-    isAutoplay: false,
-    isLooping: true,
-    onslideend:function(){
-        var id = islider2.slideIndex;
-        $(".p2 .name-list p").eq(id).addClass("cur").siblings().removeClass("cur");
-
-    }
-    //isVertical: true// 是否垂直滚动
-});
+var list_fun = new LIST();
+/*set watch_list*/
+list_fun.set_watch_list();
+/*set star_list*/
+list_fun.set_star_list();
+/*set guests_list*/
+list_fun.set_guests_list();
+/*set icon_list*/
+list_fun.set_icon_list();
+//list_fun
 $(".cover_scanning").on("touchend",function(e){
     $(e.target).addClass("cur").find(".scan_line").hide();
     setTimeout(function(){
